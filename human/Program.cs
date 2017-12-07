ae1fb403fe43c2d3be53119d7fa29fcbb3ee0719
Darby Vranek <lyra@Darbys-MacBook-Pro.local>
2017-12-07 13:11:33 -0800

    finished human

---
+++ human/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Human
+    {
+        public string Name;
+        public int Strength = 3;
+        public int Intelligence = 3;
+        public int Dexterity = 3;
+        public int Health = 100;
+
+
+        public void Attack(object other)
+        {
+            if (other is Human)
+            {
+                Human other_human = other as Human;
+                other_human.Health -= 5 * Strength;
+            }
+        }
+
+
+        public Human(string name)
+        {
+            Name = name;
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health)
+        {
+            Name = name;
+            Strength = strength;
+            Intelligence = intelligence;
+            Dexterity = dexterity;
+            Health = health;
+        }
+    }
+}
